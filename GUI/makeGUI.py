1add4d245373e4688587544a9fcced50ed008e91
mojoblue <sh18542@naver.com>
2017-10-21 23:04:01 +0900

    17.10.21 - Image Recognizer - GUI version 0

--- GUI/makeGUI.py
+++ GUI/makeGUI.py
@@ -2,37 +2,59 @@ import os
 import sys
 
 from PIL import Image
-from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QPushButton, QFileDialog, QLabel
-from PyQt5.QtWidgets import (QDialog,
-                             QVBoxLayout)
-
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+import usingKeras.loadModel as uk
 
 class Dialog(QDialog):
 
     def __init__(self):
         super(Dialog, self).__init__()
-        default_image  = os.getcwd() + '/default.jpg'
-        self.sampleImgPath = './sampleImg.jpg'
-        self.sampleImgSize = (300, 300)
+        self.modelPath = self.setModelPath('../models/4_classes_building_image.h5')
 
-        self.resizeImg(default_image, self.sampleImgSize, self.sampleImgPath)
+
+    def makeForm(self, default_image, sampleImgSize, sampleImgPath):
+        self.default_image, self.sampleImgSize, self.sampleImgPath\
+            = default_image, sampleImgSize, sampleImgPath
+        self.loadFileName = default_image
+        self.resizeImg(default_image, sampleImgSize, sampleImgPath)
         self.label = QLabel(self)
-        self.pixmap = QPixmap('./sampleImg.jpg')
+        self.pixmap = QPixmap(sampleImgPath)
         self.label.setPixmap(self.pixmap)
 
+        #Image Load용 버튼
         loadButton = QPushButton("Load Image")
+        #Image 인식용 버튼
         recogButton = QPushButton("Recognize")
+        #버튼 클릭시 기능을 추가
         loadButton.clicked.connect(self.on_click)
-
+        recogButton.clicked.connect(self.on_click_recognition)
+        #레이아웃 생성 및 항목 추가
+        layout = QFormLayout()
         self.mainLayout = QVBoxLayout()
         self.mainLayout.addWidget(self.label)
         self.mainLayout.addWidget(loadButton)
         self.mainLayout.addWidget(recogButton)
 
+        #인식결과 출력 label
+        self.text_label = QLabel(self)
+        self.text_label.setAlignment(Qt.AlignCenter)
+        self.text_label.setText("인식 결과 : ")
+        self.mainLayout.addWidget(self.text_label)
+
         self.setLayout(self.mainLayout)
         self.setWindowTitle("이미지 인식기")
+
+    def setModelPath(self, path):
+        self.modelPath = path
+
+    def setSampleImagePath(self, path):
+        self.sampleImgPath = path
+
+    # 모델을 불러오는 함수
+    def loadModel(self):
+        self.model = uk.load_model(self.modelPath)
 
     def resizeImg(self, imgPath, size, savePath):
         im = Image.open(imgPath)
@@ -41,17 +63,19 @@ class Dialog(QDialog):
 
     # 불러온 이미지를 프로그램에서 보여주기 위한 함수
     def reLoadDialog(self):
-        self.pixmap = QPixmap('./sampleImg.jpg')
+        self.pixmap = QPixmap(self.sampleImgPath)
         self.label.setPixmap(self.pixmap)
 
-
+    # 파일 선택기 함수
     def openFileNameDialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "",
                                                       "All Files (*);;JPG Image Files (*.jpg);;PNG Image Files (*.png);;GIF Image Files (*.gif)",
                                                       options=options)
+        # print(fileName)
         if fileName:
+            self.loadFileName = fileName
             self.resizeImg(fileName, self.sampleImgSize, self.sampleImgPath)
             pixmap = QPixmap(self.sampleImgPath)
             self.reLoadDialog()
@@ -63,9 +87,18 @@ class Dialog(QDialog):
 
     @pyqtSlot()
     def on_click_recognition(self):
-        print('recognize')
+        resizedLoadImagePath = './resizedLoadImg.jpg'
+        self.resizeImg(self.loadFileName, (150, 150), resizedLoadImagePath)
+        pred = uk.recognizeImage(self.model, resizedLoadImagePath)
+        self.text_label.setText("인식 결과 : {}".format(pred))
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     dialog = Dialog()
+    dialog.setModelPath('../models/4_classes_building_image.h5')
+    dialog.loadModel()
+    default_image = os.getcwd() + '/default.jpg'
+    sampleImgPath = './sampleImg.jpg'
+    sampleImgSize = (300, 300)
+    dialog.makeForm(default_image, sampleImgSize, sampleImgPath)
     sys.exit(dialog.exec_())
--- usingKeras/loadModel.py
+++ usingKeras/loadModel.py
@@ -4,14 +4,31 @@ Created on 2017. 9. 13.
 @author: 이세희
 '''
 
-from keras.models import load_model
-import Graduation_work.usingKeras.loadData as ld
+import usingKeras.loadData as ld
 import numpy as np
-import matplotlib.pyplot as plt
+from keras.models import load_model
+from PIL import Image
+
+tag = {0:'개선문', 1: '빅 벤', 2: '브란덴부르크 문', 3: '에펠탑'}
+
+def loadKerasModel(modelPath):
+    model = load_model(modelPath)
+    print('Model Loading Finished!')
+    return model
+
+def recognizeImage(model, imgPath):
+    im = Image.open(imgPath)
+    im.load()
+    inputImg = np.asarray(im, dtype='int32')
+    inputImg = inputImg.reshape((1, ) +inputImg.shape)
+    pred = model.predict_classes(inputImg)
+    return tag[pred[0]]
+    # print(x_hat.shape)
+
+
 
 if __name__ == '__main__':
 #     model = load_model
-    tag = {0:'개선문', 1: '빅 벤', 2: '브란덴부르크 문', 3: '에펠탑'}
 #     train_generator = ld.getTrain_Generator('../images/buildings/train', 2)
     test_generator = ld.getTest_Generator('Graduation_work/made_images/test/', 50)
     #Image generator의 batch_size를 가져옴
